5f94f50e9d9727abfa0e2d4358cc6063d3cc07a1
xutianji <xutianji@chinatelecom.cn>
2025-10-24 09:25:37 +0800

    Revert "Merge pull request #1 from shengyun-zhou/dev/fuse-opt"
    This reverts commit cd89f89f421f5d18bc62ca6301f410bc1b040b38, reversing
    changes made to a86942eb5df66010ba6b61f1b34d9da139fc63ad.

--- dokan_fuse/include/fusemain.h
+++ dokan_fuse/include/fusemain.h
@@ -233,7 +233,6 @@ class impl_file_handle
 	friend class impl_file_locks;
 	bool is_dir_;
 	int open_flags_;
-    uint32_t win_access_mode_;
 	uint64_t fh_;
 	impl_file_handle *next_file;
 	impl_file_lock *file_lock;
@@ -252,8 +251,6 @@ public:
 	const std::string &get_name() const { return file_lock->get_name(); }
 	void set_open_flags(int open_flags) { open_flags_ = open_flags; };
 	int open_flags() { return open_flags_; };
-    void set_win_access_mode(uint32_t access_mode) { win_access_mode_ = access_mode; }
-    uint32_t win_access_mode() const { return win_access_mode_; }
 	void set_finfo(const fuse_file_info& finfo) { fh_ = finfo.fh; };
 	int check_lock(long long start, long long len) { return file_lock->lock_file(this, start, len, false); }
 	int lock(long long start, long long len) { return file_lock->lock_file(this, start, len); }
