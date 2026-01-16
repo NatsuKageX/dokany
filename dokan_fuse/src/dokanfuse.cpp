b09f5bc1c9596f348c18c917377d4b582e9c5447
zhoushengy <zhoushengy@chinatelecom.cn>
2026-01-16 09:09:33 +0800

    FUSE - fix UNC name convert

--- dokan_fuse/src/dokanfuse.cpp
+++ dokan_fuse/src/dokanfuse.cpp
@@ -520,8 +520,9 @@ int do_fuse_loop(struct fuse *fs, bool mt) {
     if (fs->conf.networkDrive) {
       dokanOptions->Options |= DOKAN_OPTION_NETWORK;
       if (fs->conf.uncname) {
-        mbstowcs(uncName, fs->conf.uncname, MAX_PATH);
-        dokanOptions->UNCName = uncName;
+        if (utf8_to_wchar_buf(fs->conf.uncname, uncName, MAX_PATH) != -1) {
+          dokanOptions->UNCName = uncName;
+        }
       }
     }
   }
